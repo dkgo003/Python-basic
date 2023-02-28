e8afeb5090eda2b81c98c6ae0e8d521a91c8efd7
Inkyu Lee <inklee@c8r8s5.42seoul.kr>
2023-02-28 10:11:20 +0900

    모듈 연습

---
+++ module_test.py
@@ -0,0 +1,20 @@
+# 모듈 사용 실습
+
+import sys
+
+print(sys.path)
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+# sys.path.append('/Users/inklee/Desktop/math') # 코드 상에서만 가능 
+											  # 영구적으로 하려면 구글에 검색후 등록 (환경변수 등)
+
+# print(sys.path)
+
+# import test_module
+
+# 모듈 사용
+# print(test_module.power(10, 3))
+
+import chapter06_02
